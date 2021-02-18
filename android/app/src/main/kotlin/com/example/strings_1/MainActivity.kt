bc9d55a04ca61279d1264793732ba3e24e801ad7
unknown <vamshi170349@gmail.com>
2021-02-18 10:58:38 +0530

    First Task

---
+++ android/app/src/main/kotlin/com/example/strings_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.strings_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/rendering.dart';
+
+void main() {
+
+  String firstname = 'hii brr';
+  String lastname = 'i am here to help you';
+  int length = firstname.length;
+  print(length);
+  print("${firstname},${lastname}");
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[
+
+  String firstname = 'hii brr';
+  String lastname = 'i am here to help you';
+  int length = firstname.length;
+  print(length);
+  print ("${firstname},${lastname}");
+
+
+
+
+]
